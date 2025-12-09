572a8f1c7e94615a95c93fa4351c54fe90bb8643
RetroAND <jlopezm@andorra.ad>
2025-12-09 14:37:53 +0100

    Added CR-124 to device list

--- src/mame/comelta/cr124.cpp
+++ src/mame/comelta/cr124.cpp
@@ -32,6 +32,16 @@ namespace
 			required_memory_region m_ram;
 			required_device<6522via_device> m_via1;
 			required_device<6522via_device> m_via2;
+
+			void mem_map(address_map &map) ATTR_COLD;
+	}
+
+	void cr124_state::mem_map(address_map &map)
+	{
+		map(0x0000, 0x03ff).ram();
+		map(0x0400, 0x040f).m(m_via1, FUNC(via6522_device::map)).mirror(0xf0);
+		map(0x0800, 0x080f).m(m_via2, FUNC(via6522_device::map)).mirror(0xf0);
+		map(0x8000, 0xffff).rom().region("maincpu", 0);
 	}
 
 	//This is the constructor of the class
@@ -39,6 +49,11 @@ namespace
 	void cr124_state::cr124(machine_config &config)
 	{
 		W65C02S(config, m_maincpu, 1'000'000);
+		m_maincpu->set_addrmap(AS_PROGRAM, &cr124_state::mem_map);
+
+		MOS6522(config, m_via1, 1'000'000);
+
+		MOS6522(config, m_via2, 1'000'000);
 	}
 
 	void cr124_state::machine_start()
@@ -51,8 +66,17 @@ namespace
 
 	}
 
+	//**************************************************************************
+	//  INPUT PORT DEFINITIONS
+	//**************************************************************************
+
+	static INPUT_PORTS_START( cr124 )
+	INPUT_PORTS_END
+
 	ROM_START (cr124)
-		ROM_SYSTEM_BIOS(0,"diagnostics","BP-124 Diagnostics (2025)")
+		ROM_REGION(0x8000, "maincpu", 0)
+		ROM_LOAD("crc.bin", 0x8000, 0x0004, CRC(2d71bc67) SHA1(999f478cda5dc6b9da845ae8580789da6292fc75))
+		ROM_LOAD("bp124.bin", 0x8004, 0xfffc, CRC(2d71bc67) SHA1(999f478cda5dc6b9da845ae8580789da6292fc75))
 	ROM_END
 
 }
